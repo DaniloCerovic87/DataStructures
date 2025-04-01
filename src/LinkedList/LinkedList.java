1547ab004f1bf6cf6bfe7000d6aee73d278c5350
Danilo <chessteacher87@gmail.com>
2025-04-01 14:42:22 +0200

    Added removeLast method

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -92,5 +92,28 @@ public class LinkedList {
         return temp;
     }
 
+    public Node removeLast() {
+        if (length == 0) {
+            return null;
+        }
+
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
+
 }
 
