f892212e7e2c5211c181c15c24e2a43bde0d571f
Danilo <chessteacher87@gmail.com>
2025-04-18 14:00:45 +0200

    Added solution for reversing doubly linked list

---
+++ src/DoublyLinkedList/problems/problem02_reverse/Main.java
@@ -0,0 +1,14 @@
+package DoublyLinkedList.problems.problem02_reverse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList ll = new DoublyLinkedList(1);
+        ll.append(2);
+        ll.append(3);
+        ll.append(4);
+
+        ll.reverse();
+        ll.printAll();
+    }
+}
