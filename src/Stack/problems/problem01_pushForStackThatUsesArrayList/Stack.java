475c60a032dac714b183fab8997d2b6e9fd6d2d9
Danilo <chessteacher87@gmail.com>
2025-04-21 20:11:04 +0200

    Stack - added problem push for a stack that uses an array list

---
+++ src/Stack/problems/problem01_pushForStackThatUsesArrayList/Stack.java
@@ -0,0 +1,40 @@
+package Stack.problems.problem01_pushForStackThatUsesArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack<T> {
+
+    private List<T> stackList = new ArrayList<>();
+
+    public List<T> getStackList() {
+        return stackList;
+    }
+
+    public void printStack() {
+        for (T element : stackList) {
+            System.out.println(element);
+        }
+    }
+
+    public boolean isEmpty() {
+        return stackList.isEmpty();
+    }
+
+    public T peek() {
+        if (stackList.isEmpty()) {
+            return null;
+        } else {
+            return stackList.get(stackList.size() - 1);
+        }
+    }
+
+    public int size() {
+        return stackList.size();
+    }
+
+    public void push(T value) {
+        stackList.add(value);
+    }
+
+}
