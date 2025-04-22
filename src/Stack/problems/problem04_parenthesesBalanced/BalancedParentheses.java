fef779f9b0e2e21d0275fa44fceedf8287c77f9e
Danilo <chessteacher87@gmail.com>
2025-04-22 08:36:37 +0200

    Stack - added problem balanced parentheses

---
+++ src/Stack/problems/problem04_parenthesesBalanced/BalancedParentheses.java
@@ -0,0 +1,28 @@
+package Stack.problems.problem04_parenthesesBalanced;
+
+import java.util.Stack;
+
+public class BalancedParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(isBalancedParentheses("((()))"));
+        System.out.println(isBalancedParentheses("(()())"));
+        System.out.println(isBalancedParentheses("((()())"));
+    }
+
+    public static boolean isBalancedParentheses(String parentheses) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : parentheses.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
