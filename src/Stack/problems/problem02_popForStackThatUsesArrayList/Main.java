2535d2c37b2cda0dc9147a4d1dfdab026be26a97
Danilo <chessteacher87@gmail.com>
2025-04-22 07:55:32 +0200

    Stack - added problem pop for a stack that uses an array list

---
+++ src/Stack/problems/problem02_popForStackThatUsesArrayList/Main.java
@@ -0,0 +1,23 @@
+package Stack.problems.problem02_popForStackThatUsesArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        stack.printStack();
+        System.out.println("Peek: " + stack.peek());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        stack.printStack();
+        System.out.println("Peek: " + stack.peek());
+    }
+}
---
+++ src/Stack/problems/problem02_popForStackThatUsesArrayList/Stack.java
@@ -0,0 +1,48 @@
+package Stack.problems.problem02_popForStackThatUsesArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack<T> {
+
+    private List<T> stackList = new ArrayList<>();
+
+    public List<T> getStackList() {
+        return stackList;
+    }
+
+    public void printStack() {
+        for (T element : stackList) {
+            System.out.println(element);
+        }
+    }
+
+    public boolean isEmpty() {
+        return stackList.isEmpty();
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return stackList.get(stackList.size() - 1);
+        }
+    }
+
+    public int size() {
+        return stackList.size();
+    }
+
+    public void push(T value) {
+        stackList.add(value);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return stackList.remove(stackList.size() - 1);
+    }
+
+}
