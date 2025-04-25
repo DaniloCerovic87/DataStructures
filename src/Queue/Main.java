8aa9ec7acd8e37aa99be19848d8419504d5719d0
Danilo <chessteacher87@gmail.com>
2025-04-25 11:54:46 +0200

    Queue - added method enqueue

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -4,9 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         Queue queue = new Queue(7);
-        System.out.println(queue.getFirst().value);
-        System.out.println(queue.getLast().value);
-        System.out.println(queue.getLength());
+        System.out.println("First: " + queue.getFirst().value);
+        System.out.println("Last: " + queue.getLast().value);
+        System.out.println("Length: " + queue.getLength());
+
+        queue.printQueue();
+
+        queue.enqueue(8);
+        queue.enqueue(9);
+        System.out.println("First: " + queue.getFirst().value);
+        System.out.println("Last: " + queue.getLast().value);
+        System.out.println("Length: " + queue.getLength());
 
         queue.printQueue();
     }
--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -44,4 +44,15 @@ public class Queue {
 		return last;
 	}
 
+	public void enqueue(int value) {
+		Node newNode = new Node(value);
+		if (length == 0) {
+			first = newNode;
+		} else {
+			last.next = newNode;
+		}
+		last = newNode;
+		length++;
+	}
+
 }
