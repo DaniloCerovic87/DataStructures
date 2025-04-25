622b0ab010d731a9ed8a9453dd4ae37c1a45676a
Danilo <chessteacher87@gmail.com>
2025-04-25 11:06:24 +0200

    Queue - created class with constructor and getters formating

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -2,13 +2,13 @@ package Queue;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Queue queue = new Queue( 7 );
-		System.out.println(queue.getFirst().value);
-		System.out.println(queue.getLast().value);
-		System.out.println(queue.getLength());
+    public static void main(String[] args) {
+        Queue queue = new Queue(7);
+        System.out.println(queue.getFirst().value);
+        System.out.println(queue.getLast().value);
+        System.out.println(queue.getLength());
 
-		queue.printQueue();
-	}
+        queue.printQueue();
+    }
 
 }
