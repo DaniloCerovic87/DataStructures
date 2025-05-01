884003f177a3b3175395ea8400265ea30d2ba245
Danilo <chessteacher87@gmail.com>
2025-05-01 09:19:06 +0200

    BinarySearchTree added with default constructor

---
+++ src/Tree/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package Tree;
+
+public class BinarySearchTree {
+
+    private Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+}
---
+++ src/Tree/Main.java
@@ -0,0 +1,10 @@
+package Tree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+        System.out.println("Root " + myBST.getRoot());
+    }
+
+}
