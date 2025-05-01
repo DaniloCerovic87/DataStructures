e7e6422b9b74fe708b27b87ab8486df847cafed9
Danilo <chessteacher87@gmail.com>
2025-05-01 09:39:45 +0200

    BinarySearchTree main method additional info is insert was successful

--- src/Tree/Main.java
+++ src/Tree/Main.java
@@ -6,9 +6,10 @@ public class Main {
         BinarySearchTree myBST = new BinarySearchTree();
         System.out.println("Root " + myBST.getRoot());
 
-        myBST.insert(2);
-        myBST.insert(1);
-        myBST.insert(3);
+        System.out.println("Added 2: " + myBST.insert(2));
+        System.out.println("Added 1: " + myBST.insert(1));
+        System.out.println("Added 3: " + myBST.insert(3));
+        System.out.println("Try to add 3 again: " + myBST.insert(3));
 
         System.out.println("Root: " + myBST.getRoot().value);
         System.out.println("First left: " + myBST.getRoot().left.value);
