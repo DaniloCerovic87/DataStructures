50ca2eb33915d523405df278a04343f62ae1e2b7
Danilo <chessteacher87@gmail.com>
2025-05-01 09:45:04 +0200

    BinarySearchTree added method contains

--- src/Tree/Main.java
+++ src/Tree/Main.java
@@ -14,6 +14,11 @@ public class Main {
         System.out.println("Root: " + myBST.getRoot().value);
         System.out.println("First left: " + myBST.getRoot().left.value);
         System.out.println("First right: " + myBST.getRoot().right.value);
+
+        System.out.println("Contains 1: " + myBST.contains(1));
+        System.out.println("Contains 2: " + myBST.contains(2));
+        System.out.println("Contains 3: " + myBST.contains(3));
+        System.out.println("Contains 4: " + myBST.contains(4));
     }
 
 }
