06688990fdcf8a89dd669cfc9404eaeb05d3089b
Danilo <chessteacher87@gmail.com>
2025-05-01 09:35:35 +0200

    BinarySearchTree added insert method

--- src/Tree/Main.java
+++ src/Tree/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
         System.out.println("Root " + myBST.getRoot());
+
+        myBST.insert(2);
+        myBST.insert(1);
+        myBST.insert(3);
+
+        System.out.println("Root: " + myBST.getRoot().value);
+        System.out.println("First left: " + myBST.getRoot().left.value);
+        System.out.println("First right: " + myBST.getRoot().right.value);
     }
 
 }
