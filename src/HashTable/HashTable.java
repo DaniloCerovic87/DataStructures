5b17e292e7a9898fbe307535c318def9448bb70f
Danilo <chessteacher87@gmail.com>
2025-05-01 10:25:16 +0200

    HashTable - constructor and print method created

---
+++ src/HashTable/HashTable.java
@@ -0,0 +1,34 @@
+package HashTable;
+
+public class HashTable {
+
+    private int size = 7;
+    private Node[] dataMap;
+
+    class Node {
+        String key;
+        int value;
+        Node next;
+
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public void printTable() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "=" + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+
+}
