a7b8dc6584d8aa6b2da389494a94b9361053645c
Danilo <chessteacher87@gmail.com>
2025-05-02 09:39:44 +0200

    HashMap - added problem FindDuplicates

---
+++ src/HashTable/problems/problem02_findDuplicates/FindDuplicates.java
@@ -0,0 +1,30 @@
+package HashTable.problems.problem02_findDuplicates;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        System.out.println(findDuplicates(nums));
+    }
+
+    private static List<Integer> findDuplicates(int[] nums) {
+        Map<Integer, Integer> numCounts = new HashMap<>();
+        for (int num : nums) {
+            numCounts.put(num, numCounts.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> duplicates = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : numCounts.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicates.add(entry.getKey());
+            }
+        }
+        return duplicates;
+    }
+
+}
