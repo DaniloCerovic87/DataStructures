aa2e07d66cc33a365f1f5f2389d3d12f7ce5bba8
Danilo <chessteacher87@gmail.com>
2025-05-02 10:00:33 +0200

    HashMap - find first non-repeating character added one more test case

--- src/HashTable/problemsMap/problem03_firstNonRepeatingCharacter/FirstNonRepeatingCharacter.java
+++ src/HashTable/problemsMap/problem03_firstNonRepeatingCharacter/FirstNonRepeatingCharacter.java
@@ -8,18 +8,19 @@ public class FirstNonRepeatingCharacter {
     public static void main(String[] args) {
         System.out.println(firstNonRepeatingChar("hello"));
         System.out.println(firstNonRepeatingChar("leetcode"));
+        System.out.println(firstNonRepeatingChar("aabbcc"));
     }
 
     private static Character firstNonRepeatingChar(String string) {
         Map<Character, Integer> charCounts = new HashMap<>();
-        for(int i = 0; i < string.length(); i++) {
+        for (int i = 0; i < string.length(); i++) {
             char c = string.charAt(i);
             charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
         }
 
-        for(int i = 0; i < string.length(); i++) {
+        for (int i = 0; i < string.length(); i++) {
             char c = string.charAt(i);
-            if(charCounts.get(c) == 1) {
+            if (charCounts.get(c) == 1) {
                 return c;
             }
         }
