7298493b7c3e1ff5d2a65fd5656ef72b9230d380
Danilo <chessteacher87@gmail.com>
2025-05-03 08:35:16 +0200

    HashTable - solving problem SubarraySum using prefix sum and hashmap

--- src/HashTable/problemsMap/problem06_subarraySum/SubarraySum.java
+++ src/HashTable/problemsMap/problem06_subarraySum/SubarraySum.java
@@ -1,13 +1,34 @@
 package HashTable.problemsMap.problem06_subarraySum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SubarraySum {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(subarraySumSlidingWindow(new int[]{1, 2, 3, 7, 5}, 12)));
-        System.out.println(Arrays.toString(subarraySumSlidingWindow(new int[]{1, 2, 3, 4, 5}, 20)));
+        System.out.println(Arrays.toString(subarraySumPrefixSumAndMap(new int[]{1, 2, 3, 7, 5}, 13)));
+        System.out.println(Arrays.toString(subarraySumPrefixSumAndMap(new int[]{1, 2, 3, 4, 5}, 20)));
     }
+
+    private static int[] subarraySumPrefixSumAndMap(int[] nums, int target) {
+        Map<Integer, Integer> prefixSumPositions = new HashMap<>();
+        int sum = 0;
+
+        prefixSumPositions.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (prefixSumPositions.containsKey(sum - target)) {
+                return new int[]{prefixSumPositions.get(sum - target) + 1, i};
+            }
+
+            prefixSumPositions.put(sum, i);
+
+        }
+        return new int[]{};
+    }
+
 
     // works with positive numbers only
     private static int[] subarraySumSlidingWindow(int[] nums, int target) {
