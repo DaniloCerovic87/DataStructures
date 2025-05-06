8ddf88401535426f480b98f3826229d34e9000db
Danilo <chessteacher87@gmail.com>
2025-05-06 11:42:04 +0200

    Graph - method to add edge implemented

--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
         System.out.println(graph.addVertex("A"));
+        System.out.println(graph.addVertex("B"));
+        graph.printGraph();
+        graph.addEdge("A", "B");
         graph.printGraph();
     }
 
