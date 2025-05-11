57435217dc41c4c83b2af1f9cd9f12fa2c130ec8
Danilo <chessteacher87@gmail.com>
2025-05-11 09:47:57 +0200

    Problem MinHeap with insert method

---
+++ src/Heap/problems/problem01_MinHeap/Main.java
@@ -0,0 +1,15 @@
+package Heap.problems.problem01_MinHeap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap();
+        minHeap.insert(3);
+        minHeap.insert(4);
+        minHeap.insert(5);
+        minHeap.insert(2);
+
+        System.out.println(minHeap.getHeap());
+    }
+
+}
